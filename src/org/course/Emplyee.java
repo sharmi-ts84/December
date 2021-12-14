d5438810ec0ba5399701625d18b9475ace4f10aa
sharmi-ts84 <sharmi.ts84@gmail.com>
2021-12-14 11:24:15 +0530

    first commit

---
+++ src/org/course/Emplyee.java
@@ -0,0 +1,17 @@
+package org.course;
+
+public class Emplyee {
+
+	public static void main(String[] args) {
+		
+	int age = 10;
+	
+	if (age >18) {
+		System.out.println("Eligible to Vote");
+	}
+	
+	else {
+		System.out.println("Not Eligible to Vote");
+	}
+	}
+}
